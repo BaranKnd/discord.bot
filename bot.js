0c7ce1d4ba5bc50df46077dd2f8c3088b5b75e3a
BaranKnd <45537716+BaranKnd@users.noreply.github.com>
2018-12-02 18:12:37 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log(`Botun olan ${client.user.tag}!Sunucuya giriş yaptı ve artık aktif!`);
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+    msg.reply('Aleyküm Selam');
+  }
+  if (msg.content.toLowerCase() === 'pp') {
+    msg.channel.send(msg.author.avatarURL);
+  }
+  if (msg.content.toLowerCase() === 'muhammed') {
+    msg.reply('Muhammed abim kral adamdır kendisi çok insaflı birisidir kesinlikle kimseyi dolandırmaz');
+  }
+
+});
+
+client.login('NTE4NDIyNDkwMTA0NTk0NDM0.DuQk7g.H46JtIPRHGLnR5QC0nRyonb5YT8');
